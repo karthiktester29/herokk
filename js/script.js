e858bf0bdbc83d42d1cbbe4f86393e5e78789cd2
cherylcourt <cheryl.court@enbridge.com>
2015-01-19 16:50:53 -0700

    Added possible code to run (city and street are not being selected...coming out undefined for some reason)

--- js/script.js
+++ js/script.js
@@ -14,10 +14,19 @@ function loadData() {
     // load streetview
 
     // YOUR CODE GOES HERE!
+    //var $street = $(".street").val();
+    //var $city = $(".city").val();
+    //
+    //var streetviewUrl = '<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' +
+    //        $street + ', ' + $city + '">';
+    //
+    //$body.append(streetviewUrl);
 
     return false;
-};
+}
 
 $('#form-container').submit(loadData);
+
+
 
 // loadData();
