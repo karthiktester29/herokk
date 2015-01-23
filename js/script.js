a42d5d0f4decf37539107afe790ab1a1e23ce8e1
Cheryl Court <cheryl.court@gmail.com>
2015-01-22 22:08:18 -0700

    Added NYTimes articles to web page

--- js/script.js
+++ js/script.js
@@ -16,11 +16,34 @@ function loadData() {
     // YOUR CODE GOES HERE!
     var $street = $("#street").val();
     var $city = $("#city").val();
+    var address = $street + ', ' + $city;
+
+    $greeting.text('So, you want to live at '+ address +'?');
+    $nytHeaderElem.text("New York Times Articles About "+ $city);
 
     var streetviewUrl = '<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' +
-            $street + ', ' + $city + '">';
+           address + '">';
 
     $body.append(streetviewUrl);
+
+    //&fq=news_desk:("Sports") AND glocations:("NEW YORK CITY")
+    $.getJSON("http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+$city+"&sort=newest&api-key=<insert key here>", function( data ) {
+        var docs = data.response.docs;
+        var articles = [];
+
+        $.each(docs, function(index, value){
+            if(value.document_type == 'article') {
+                articles.push('<li class="article"><a href="'+value.web_url+'">'+value.headline.main+'</a><p>' + value.snippet + '</p>');
+            }
+
+        });
+
+        $(articles.join( " " )).appendTo( $("#nytimes-articles") );
+
+        // udacity solution
+        // for(...)
+        //   $nytElem.append(<article list item>);
+    });
 
     return false;
 }
